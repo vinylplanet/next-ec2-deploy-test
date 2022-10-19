84b7d1891b855c3c7650b9b8b345ccc45672db38
vinylplanet <vinyl.planet.ar@gmail.com>
2022-10-19 16:13:16 -0300

    cambio en el texto

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! TEST</a>
+          Welcome to <a href="https://nextjs.org">Next.js! TEST 2</a>
         </h1>
 
         <p className={styles.description}>
