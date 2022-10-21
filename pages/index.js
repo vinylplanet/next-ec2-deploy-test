4e53a7f4c31d6dee34e3caf2b536b84a36ab4366
vinylplanet <vinyl.planet.ar@gmail.com>
2022-10-21 16:37:42 -0300

    force deploy

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! TEST 4</a>
+          Welcome to <a href="https://nextjs.org">Next.js! TEST 5</a>
         </h1>
 
         <p className={styles.description}>
